f46fd48f6eee1bc56687712e085d0542d8b1319e
thomas <tmeyerwebdev@gmail.com>
2021-09-21 21:01:55 +0000

    Refactor and cleaned up code

---
+++ typerwriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(()=>{
+    process.stdout.write(sentence[i]);
+  }, i * 50);
+}
+
+setTimeout(()=> {
+  console.log("\n");
+}, 1600);
